b6401dc2b0c29e1a984cbbe65d9356580a513d08
Colderz <arekzim98@gmail.com>
2020-11-11 19:49:14 +0100

    Add new activity, change some idea, create editablecard

---
+++ app/src/main/java/pakiet/arkadiuszzimny/extralessonapp1/EditableListActivity.kt
@@ -0,0 +1,11 @@
+package pakiet.arkadiuszzimny.extralessonapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditableListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editable_list)
+    }
+}
